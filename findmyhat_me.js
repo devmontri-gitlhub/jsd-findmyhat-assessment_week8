8fbba819b58664da43478720f1329f155c700298
devmontri-gitlhub <dev.montri@gmail.com>
2026-04-24 14:01:15 +0700

    v1.2: Add Mode Game And Check Other Bug

--- findmyhat_me.js
+++ findmyhat_me.js
@@ -16,13 +16,13 @@ const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
  //////////////////////////////////////////////////////////////////////////////     
  //////////////////////////////////////////////////////////////////////////////          
 /*------------------- Start : Creating Functions -------------------------*/
-async function actionChoice(choice) {
+async function fnActionChoice(choice) {
                             console.clear();
 
                                     if (choice==='1') {
                                         console.log('คุณเลือก: ' + choice + ' คุณได้เลือกเริ่มเล่นเกม');
                                         const name = prompt('กรุณากรอกชื่อตัวละครของคุณค่ะ >> '); 
-                                        console.log('ยินดีต้อนรับเข้าสู่เกม Find Your Hat, ' + name + '!');
+                                        return name;
                                     }
 
                                     else if (choice === '2') {
@@ -32,18 +32,63 @@ async function actionChoice(choice) {
                                      await delay(3000);
                                      process.exit();
                                     }
+}
+
+async function fnTypeGame() {
+                            console.clear()
+                            console.log('กรุณาเลือกรูปแบบเกมที่คุณต้องการเล่นค่ะ');
+                            console.log('1. ฝึกเล่นเกม (Traning Game)');
+                            console.log('2. เล่นเกม Find My Hat  (Custom Mode)');
+                            const typegame = prompt('เลือกรายการ (1 หรือ 2) >> ');
+
+                            if (typegame === '1') {
+                                 console.log('คุณเลือก: ' + typegame + ' คุณได้เลือกฝึกเล่นเกม (Training Game)');
+                                 return typegame;
+                            } else if (typegame === '2') {
+                                console.log('คุณเลือก: ' + typegame + ' คุณได้เลือกเล่นเกม Find My Hat (Custom Mode)');
+                                return typegame;
+                            } else if (typegame !== '1' && typegame !== '2') {
+                                    console.log('คุณเลือก: ' + typegame + ' คุณได้เลือกตัวเลือกที่ไม่ถูกต้อง กรุณาเลือกรายการที่ถูกต้องค่ะ (โปรแกรมจะกลับไปยังเมนูเลือกรูปแบบเกมใน 3 วินาที)');
+                                        await delay(3000);
+                                     return await fnTypeGame();
+                            } else {
+                               return 'คุณเลือก: ' + typegame + ' คุณได้เลือกตัวเลือกที่ไม่ถูกต้อง กรุณาเลือกรายการที่ถูกต้องค่ะ (โปรแกรมจะกลับไปยังเมนูเลือกรูปแบบเกมใน 3 วินาที)';
+                                
                             }
+                        }
+
+async function fnTrialGame(typegame) {
+                            console.clear();
+                            if (typegame === '1') {
+                                console.log('คุณได้เข้าสู่โหมดฝึกเล่นเกม (Training Game) ขอให้สนุกกับการฝึกเล่นเกมนะค่ะ!');
+                            } else if (typegame === '2') {
+                                console.log('คุณได้เข้าสู่โหมดเล่นเกม Find My Hat (Custom Mode) ขอให้สนุกกับการเล่นเกมนะค่ะ!');
+                            }
+                        }
+
 
 /*------------------- END : Creating Functions -------------------------*/
 ///////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
 /*--------------------- Start : Main Program ----------------------------*/  
 console.clear()
+
 console.log('=== ยินดีตอนรับเข้าสู๋ Find Your Hat ===');
 console.log('1. เริ่มเกม (Start Game)');
 console.log('2. ออกจากเกม (Exit)');
 const choice = prompt('เลือกรายการ (1 หรือ 2) >> ');
-await actionChoice(choice);
+const name = await fnActionChoice(choice);
+console.log('ยินดีต้อนรับเข้าสู่เกม Find Your Hat, ' + name + '!');
+
+const typegame = await fnTypeGame();
+
+const value = await fnTrialGame(typegame);
+
+
+
+
+
+
 
 /*----------------------- END : Main Program ---------------------------*/
 ///////////////////////////////////////////////////////////////////////////////
