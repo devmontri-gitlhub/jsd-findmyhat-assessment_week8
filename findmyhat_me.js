68e6ae600a8bae6eb3128998501656f7412a3409
devmontri-gitlhub <dev.montri@gmail.com>
2026-04-24 10:23:10 +0700

    v1.0 Send K-Gun About Test Week8

---
+++ findmyhat_me.js
@@ -0,0 +1,8 @@
+/*----------- Start : Importing Modules -----------*/
+import promptSync from 'prompt-sync';
+
+
+/*----------- End : Importing Modules -----------*/
+
+
+                const prompt = promptSync();
